423cad55a94a661e4f48fea7a858ab5060a0644b
Tara Z. Manicsic <tzmanics@gmail.com>
2020-04-24 23:56:26 -0400

    adds conditional input section

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
-module.exports = {
-  onInit: () => {
-    console.log('onInit: I run before anything else 🐣');
-  },
-  onPreBuild: () => {
-    console.log('onPreBuild: I run_before_ build commands are executed 🌤');
-  },
-  onBuild: ({ inputs }) => {
-    console.log('onBuild: I run while build commands are being executed ⚙️');
-    console.log(`I also know your keyword is: ${inputs.keyword}.`);
-    console.log(`Oh, and your database URL is: ${inputs.databaseUrl}.`);
-  },
-  onPostBuild: ({ constants }) => {
-    console.log('onPostBuild: I run _after_ build commands are executed ✅');
-    console.log('Here are some other things I know thanks to constants:');
-    console.log(constants);
-  },
-  onSuccess: ({ utils }) => {
-    console.log('onSuccess: I run on build success 🎉');
-    /*
+module.exports = function runPlugin(inputs) {
+  if (!inputs.triggerAll) {
+    return {
+      onInit: () => {
+        console.log(`triggerAll set to ${inputs.triggerAll}, no fun 🤷🏻‍♀️!`);
+      },
+    };
+  } else {
+    return {
+      onInit: () => {
+        console.log('onInit: I run before anything else 🐣');
+      },
+      onPreBuild: ({ inputs }) => {
+        console.log('onPreBuild: I run_before_ build commands are executed 🌤');
+      },
+      onBuild: ({ inputs }) => {
+        console.log(
+          'onBuild: I run while build commands are being executed ⚙️'
+        );
+        console.log(`I also know your keyword is: ${inputs.keyword}.`);
+        console.log(`Oh, and your database URL is: ${inputs.databaseUrl}.`);
+      },
+      onPostBuild: ({ constants }) => {
+        console.log(
+          'onPostBuild: I run _after_ build commands are executed ✅'
+        );
+        console.log('Here are some other things I know thanks to constants:');
+        console.log(constants);
+      },
+      onSuccess: ({ utils }) => {
+        console.log('onSuccess: I run on build success 🎉');
+        /*
     // Uncomment this block to see the onError code run.
     //  This is also a good way to handle errors 👇
     try {
@@ -28,13 +40,15 @@ module.exports = {
       // utils.build.cancelBuild(`This will cancel the build ${error}.`);
     }
     */
-  },
-  onError: () => {
-    console.log('onError: I run on build error 🚒');
-  },
-  onEnd: () => {
-    console.log(
-      'onEnd: I run on build error or success when the build process ends 🎬'
-    );
-  },
+      },
+      onError: () => {
+        console.log('onError: I run on build error 🚒');
+      },
+      onEnd: () => {
+        console.log(
+          'onEnd: I run on build error or success when the build process ends 🎬'
+        );
+      },
+    };
+  }
 };
