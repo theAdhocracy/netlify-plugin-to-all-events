343f11616d551fdab14dcbed4ada28eb1cd8eee1
Tara Z. Manicsic <tzmanics@gmail.com>
2020-04-24 22:15:57 -0400

    switches single quotes, adds errors and comments out errors test, plus moves

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 module.exports = {
   onInit: () => {
-    console.log("onInit: I run before anything else 🐣");
+    console.log('onInit: I run before anything else 🐣');
   },
   onPreBuild: () => {
-    console.log("onPreBuild: I run_before_ build commands are executed 🌤");
+    console.log('onPreBuild: I run_before_ build commands are executed 🌤');
   },
   onBuild: ({ inputs }) => {
-    console.log("onBuild: I run while build commands are being executed ⚙️");
+    console.log('onBuild: I run while build commands are being executed ⚙️');
     console.log(`I also know your keyword is: ${inputs.keyword}.`);
     console.log(`Oh, and your database URL is: ${inputs.databaseUrl}.`);
   },
   onPostBuild: ({ constants }) => {
-    console.log("onPostBuild: I run _after_ build commands are executed ✅");
-    console.log("Here are some other things I know thanks to constants:");
+    console.log('onPostBuild: I run _after_ build commands are executed ✅');
+    console.log('Here are some other things I know thanks to constants:');
     console.log(constants);
   },
-  onSuccess: ({ constants }) => {
-    console.log("onSuccess: I run on build success 🎉");
+  onSuccess: ({ utils }) => {
+    console.log('onSuccess: I run on build success 🎉');
+    /* Uncomment this to see the onError code run.
+       This is also a good way to handle errors 👇
     try {
-      errorTime();
+      throw new Error('This is the error 🚨');
     } catch (error) {
-      utils.build.failBuild("Oh no! Error time!", { error });
+      utils.build.failBuild('Oh no! Error time!', { error });
     }
+    */
   },
   onError: () => {
-    console.log("onError: I run on build error 🚒");
+    console.log('onError: I run on build error 🚒');
   },
   onEnd: () => {
     console.log(
-      "onEnd: I run on build error or success when the build process ends 🎬"
+      'onEnd: I run on build error or success when the build process ends 🎬'
     );
   },
 };
